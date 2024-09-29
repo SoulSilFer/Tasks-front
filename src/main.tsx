56f219bd0119946a1800e4ac5194eb42572b8b86
Vitor Fernandes <vitor.fer.sil@hotmail.com>
2024-09-29 14:09:56 -0300

    feat: firebase deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,24 @@
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.tsx';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
