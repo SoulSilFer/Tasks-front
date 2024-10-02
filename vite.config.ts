6c7ad788fa1ed4672121f0096edb129454bad088
Vitor Fernandes <vitor.fer.sil@hotmail.com>
2024-10-01 22:30:12 -0300

    its only

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [react(), svgr()],
